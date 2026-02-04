138cfe1814b0021974717cdcf897af88acada2ac
Linda Song <poohzoa@gmail.com>
2026-02-04 11:55:20 +1100

    feat: add feature to write and view comments

--- src/pages/posts/detail.tsx
+++ src/pages/posts/detail.tsx
@@ -1,6 +1,50 @@
-import PostDetail from "components/posts/PostDetail";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
 
-export default function PostDetailPage() {
-  return <PostDetail/>
+import Loader from "components/loader/Loader";
+import PostBox from "components/posts/PostBox";
+import { type PostProps } from "pages/home";
+import { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { db } from "firebaseApp";
 
+import PostHeader from "components/posts/PostHeader";
+import CommentForm from "components/comments/CommentForm";
+import CommentBox, { type CommentProps } from "components/comments/CommentBox";
+
+export default function PostDetail() {
+  const params = useParams();
+  const [post, setPost] = useState<PostProps | null>(null);
+
+  const getPost = useCallback(async () => {
+    if (params.id) {
+      const docRef = doc(db, "posts", params.id);
+      onSnapshot(docRef, (doc) => {
+        setPost({ ...(doc?.data() as PostProps), id: doc.id });
+      });
+    }
+  }, [params.id]);
+
+  useEffect(() => {
+    if (params.id) getPost();
+  }, [getPost, params.id]);
+
+  return (
+    <div className="post">
+      <PostHeader />
+      {post ? (
+        <>
+          <PostBox post={post} />
+          <CommentForm post={post} />
+          {post?.comments
+            ?.slice(0)
+            ?.reverse()
+            ?.map((data: CommentProps, index: number) => (
+              <CommentBox data={data} key={index} post={post} />
+            ))}
+        </>
+      ) : (
+        <Loader />
+      )}
+    </div>
+  );
 }
